4762ce7e0809834251aceb0bacf16b64fab29977
Jcchm24 <Jcchm2004@gmail.com>
2024-11-20 22:36:47 -0600

    Reestructurar App.vue: agregar componentes Header y Sidebar, y modificar la disposición de la interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,43 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="grid-container w-full h-screen p-5">
+    <!-- Sidebar de navegación -->
+    <Sidebar />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <!-- Header con formulario para crear rutinas -->
+    <Header />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <!-- Sección donde se muestran las vistas -->
+    <section class="content">
+      <router-view></router-view> <!-- Las vistas se inyectan aquí -->
+    </section>
+  </div>
 </template>
 
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.grid-container {
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 20px;
+  grid-template-areas:
+    "sidebar header"
+    "sidebar content";
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header {
+  grid-area: header;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.sidebar {
+  grid-area: sidebar;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content {
+  grid-area: content;
 }
 </style>
